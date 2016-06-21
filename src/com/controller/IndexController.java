e31d972ff2bae8f0e23dbad3c28fad1dfb3422a0
yursile <289181450@qq.com>
2016-06-21 15:24:36 +0800

    yingjun

---
+++ src/com/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/sp")
+public class IndexController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
